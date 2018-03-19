159b4eca3630db5110356e7ff2871d2c0bf33d68
牛康康 <1261103246@qq.com>
2018-03-19 11:58:13 +0800

    Update DefaultViewPageIndicator.js

--- DefaultViewPageIndicator.js
+++ DefaultViewPageIndicator.js
@@ -10,10 +10,74 @@ var {
   View,
   Animated,
 } = ReactNative;
+import  PropTypes from  'prop-types'
 
 var deviceWidth = Dimensions.get('window').width;
-var DOT_SIZE = 6;
-var DOT_SAPCE = 4;
+
+var DOT_SIZE = 6
+var DOT_SAPCE = 8;
+
+
+
+class DefaultViewPageIndicator extends React.Component{
+  static propTypes = {
+    goToPage: PropTypes.func,
+    activePage: PropTypes.number,
+    pageCount: PropTypes.number,
+    dotStyle: PropTypes.object,
+    selectedDotStyle: PropTypes.object,
+  }
+
+  getInitialState() {
+    return {
+      viewWidth: 0,
+    };
+  }
+
+  renderIndicator(page) {
+    //var isTabActive = this.props.activePage === page;
+    return (
+      <TouchableOpacity style={styles.tab} key={'idc_' + page} onPress={() => this.props.goToPage(page)}>
+        <View style={[styles.dot,this.props.dotStyle]} />
+      </TouchableOpacity>
+    );
+  }
+
+  render() {
+    var pageCount = this.props.pageCount;
+    var dotSize = this.props.dotSize == null ? DOT_SIZE : this.props.dotSize;//by melody
+    var itemWidth = dotSize + (DOT_SAPCE * 2);
+    var offset = (this.state.viewWidth - itemWidth * pageCount) / 2 + itemWidth * this.props.activePage;
+
+    //var left = offset;
+    var offsetX = itemWidth * (this.props.activePage - this.props.scrollOffset);
+    var left = this.props.scrollValue.interpolate({
+      inputRange: [0, 1], outputRange: [offsetX, offsetX + itemWidth]
+    })
+
+    var indicators = [];
+    for (var i = 0; i < pageCount; i++) {
+      indicators.push(this.renderIndicator(i))
+    }
+
+    return (
+      <View style={styles.tabs}
+        onLayout={(event) => {
+            var viewWidth = event.nativeEvent.layout.width;
+            
+            if (!viewWidth || this.state.viewWidth === viewWidth) {
+              return;
+            }
+            this.setState({
+              viewWidth: viewWidth,
+            });
+          }}>
+        {indicators}
+        <Animated.View style={[styles.curDot, this.props.selectedDotStyle, {left}]} />
+      </View>
+    );
+  }
+}
 
 var styles = StyleSheet.create({
   tab: {
@@ -21,6 +85,7 @@ var styles = StyleSheet.create({
   },
 
   tabs: {
+    height: 30,
     flexDirection: 'row',
     alignItems: 'center',
     justifyContent: 'center',
@@ -43,62 +108,6 @@ var styles = StyleSheet.create({
     backgroundColor: '#80ACD0',
     margin: DOT_SAPCE,
     bottom: 0,
-  },
-});
-
-var DefaultViewPageIndicator = React.createClass({
-  propTypes: {
-    goToPage: React.PropTypes.func,
-    activePage: React.PropTypes.number,
-    pageCount: React.PropTypes.number
-  },
-
-  getInitialState() {
-    return {
-      viewWidth: 0,
-    };
-  },
-
-  renderIndicator(page) {
-    //var isTabActive = this.props.activePage === page;
-    return (
-      <TouchableOpacity style={styles.tab} key={'idc_' + page} onPress={() => this.props.goToPage(page)}>
-        <View style={styles.dot} />
-      </TouchableOpacity>
-    );
-  },
-
-  render() {
-    var pageCount = this.props.pageCount;
-    var itemWidth = DOT_SIZE + (DOT_SAPCE * 2);
-    var offset = (this.state.viewWidth - itemWidth * pageCount) / 2 + itemWidth * this.props.activePage;
-
-    //var left = offset;
-    var offsetX = itemWidth * (this.props.activePage - this.props.scrollOffset);
-    var left = this.props.scrollValue.interpolate({
-      inputRange: [0, 1], outputRange: [offsetX, offsetX + itemWidth]
-    })
-
-    var indicators = [];
-    for (var i = 0; i < pageCount; i++) {
-      indicators.push(this.renderIndicator(i))
-    }
-
-    return (
-      <View style={styles.tabs}
-        onLayout={(event) => {
-            var viewWidth = event.nativeEvent.layout.width;
-            if (!viewWidth || this.state.viewWidth === viewWidth) {
-              return;
-            }
-            this.setState({
-              viewWidth: viewWidth,
-            });
-          }}>
-        {indicators}
-        <Animated.View style={[styles.curDot, {left}]} />
-      </View>
-    );
   },
 });
 
